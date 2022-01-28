249948a5583605d528c367120e41bc870566c730
tylershanks <tshanks530@gmail.com>
2022-01-27 21:00:00 -0500

    Game works completely. Some small issues, most are not important (such as the opponent taking their turn instantly). The game still works, just not pretty. I am going to go back over everything and make it much more modular, which was the goal of this project in the first place. I copy/pasted 8 times every time I changed something on one of the buttons. That is the main priority here. Besides that, it's done.

--- script.js
+++ script.js
@@ -1,102 +1,314 @@
 console.log("tic tac toe game");
 
 
+var gameBoardModule = (function() {
+    'use strict';
 
 
-//game board that everything checks to see if moves are legal,
-//game is over, etc.
-let gameBoard = [
-                    '','','',
-                    '','','',
-                    '','',''
-                ];
-
-//emptey array to easily reset the original game board
-let gameBoardRestart = [
-                    '','','',
-                    '','','',
-                    '','',''
-                ];
+    //game board that everything checks to see if moves are legal,
+    //game is over, etc.
+    let gameBoard = [
+                        0,0,0,
+                        0,0,0,
+                        0,0,0
+                    ];
 
 
-//x = 1, o = 0
+    let gameOver = false;
 
-//when button w specific ID is clicked, corresponding function takes place
-//changes button inner html, changes value of game board array to 1
-TL.onclick = function topLeft () {
-    console.log('topLeft clicked');
-    document.querySelector('#TL').innerHTML = 'X';
-    gameBoard[0] = 1;
-    console.log(gameBoard);
-}
+    //x = 1, o = 2, empty slot = 0
 
-TM.onclick = function topMiddle () {
-    console.log('topMiddle clicked');
-    document.querySelector('#TM').innerHTML = 'X';
-    gameBoard[1] = 1;
-    console.log(gameBoard);
-}
+    function computerTurn () {
+        let randomBoard = Math.floor(Math.random()*9);
 
-TR.onclick = function topRight () {
-    console.log('topRight clicked');
-    document.querySelector('#TR').innerHTML = 'X';
-    gameBoard[2] = 1;
-    console.log(gameBoard);
-}
+        if (gameBoard[randomBoard] == 0 && gameOver == false) {
+            gameBoard[randomBoard] = 2;
+            document.querySelector('#board'+randomBoard).innerHTML = 'O'; 
+        }
+        else if (gameOver == false) {
+            computerTurn();
+        }
+    }
 
-ML.onclick = function MiddleLeft () {
-    console.log('MiddleLeft clicked');
-    document.querySelector('#ML').innerHTML = 'X';
-    gameBoard[3] = 1;
-    console.log(gameBoard);
-}
+    //restart button, clears all buttons inner html, resets game board array
+    restartButton.onclick = function restartGame () {
+        console.log('restartButton clicked or game ended');
+        document.querySelector('#board0').innerHTML = '';
+        document.querySelector('#board1').innerHTML = '';
+        document.querySelector('#board2').innerHTML = '';
+        document.querySelector('#board3').innerHTML = '';
+        document.querySelector('#board4').innerHTML = '';
+        document.querySelector('#board5').innerHTML = '';
+        document.querySelector('#board6').innerHTML = '';
+        document.querySelector('#board7').innerHTML = '';
+        document.querySelector('#board8').innerHTML = '';
+        gameBoard = [
+                        0,0,0,
+                        0,0,0,
+                        0,0,0
+                    ];
+        gameOver = false;
+        document.querySelector('#topBox').innerHTML = 'Tic-Tac-Toe';
+    }
 
-MM.onclick = function middleMiddle () {
-    console.log('middleMiddle clicked');
-    document.querySelector('#MM').innerHTML = 'X';
-    gameBoard[4] = 1;
-    console.log(gameBoard);
-}
+    
 
-MR.onclick = function middleRight () {
-    console.log('middleRight clicked');
-    document.querySelector('#MR').innerHTML = 'X';
-    gameBoard[5] = 1;
-    console.log(gameBoard);
-}
+    function checkWinner () {
+        console.log('winner checked');
+        let gameBoardSum = gameBoard.reduce((a,b) => a+b, 0)
 
-BL.onclick = function bottomLeft () {
-    console.log('bottomLeft clicked');
-    document.querySelector('#BL').innerHTML = 'X';
-    gameBoard[6] = 1;
-    console.log(gameBoard);
-}
+        if (gameBoard[0] != 0 &&
+            gameBoard[0] == gameBoard[1] &&
+            gameBoard[1] == gameBoard[2]
+            ||
+            gameBoard[3] != 0 &&
+            gameBoard[3] == gameBoard[4] &&
+            gameBoard[4] == gameBoard[5]
+            ||
+            gameBoard[6] != 0 &&
+            gameBoard[6] == gameBoard[7] &&
+            gameBoard[7] == gameBoard[8]
+            ||
+            gameBoard[0] != 0 &&
+            gameBoard[0] == gameBoard[3] &&
+            gameBoard[3] == gameBoard[6]
+            ||
+            gameBoard[1] != 0 &&
+            gameBoard[1] == gameBoard[4] &&
+            gameBoard[4] == gameBoard[7]
+            ||
+            gameBoard[2] != 0 &&
+            gameBoard[2] == gameBoard[5] &&
+            gameBoard[5] == gameBoard[8]
+            ||
+            gameBoard[0] != 0 &&
+            gameBoard[0] == gameBoard[4] &&
+            gameBoard[4] == gameBoard[8]
+            ||
+            gameBoard[2] != 0 &&
+            gameBoard[2] == gameBoard[4] &&
+            gameBoard[4] == gameBoard[6]
+            
+        ) {
+            console.log('game over');
+            gameOver = true;
 
-BM.onclick = function bottomMiddle () {
-    console.log('bottomMiddle clicked');
-    document.querySelector('#BM').innerHTML = 'X';
-    gameBoard[7] = 1;
-    console.log(gameBoard);
-}
+            if (gameBoardSum == 7 || gameBoardSum == 10 || gameBoardSum == 13) {
+                console.log('x wins!')
+                document.querySelector('#topBox').innerHTML = 'X Wins!';
+            }
 
-BR.onclick = function bottomRight () {
-    console.log('bottomRight clicked');
-    document.querySelector('#BR').innerHTML = 'X';
-    gameBoard[8] = 1;
-    console.log(gameBoard);
-}
+            else if (gameBoardSum == 9 || gameBoardSum == 12)
+            {
+                console.log('o wins!')
+                document.querySelector('#topBox').innerHTML = 'O Wins!';
+            }            
+        }
 
-//restart button, clears all buttons inner html, resets game board array
-restartButton.onclick = function restartGame () {
-    console.log('restartButton clicked');
-    document.querySelector('#TL').innerHTML = '';
-    document.querySelector('#TM').innerHTML = '';
-    document.querySelector('#TR').innerHTML = '';
-    document.querySelector('#ML').innerHTML = '';
-    document.querySelector('#MM').innerHTML = '';
-    document.querySelector('#MR').innerHTML = '';
-    document.querySelector('#BL').innerHTML = '';
-    document.querySelector('#BM').innerHTML = '';
-    document.querySelector('#BR').innerHTML = '';
-    gameBoard = gameBoardRestart;
-}
+        else if (gameBoardSum == 13)
+        {
+            document.querySelector('#topBox').innerHTML = 'Tie!';
+            gameOver = true;
+        }
+    }
+
+    //when button w specific ID is clicked, corresponding function takes place
+    //changes button inner html, changes value of game board array to 1
+    board0.onclick = function topLeft () {
+
+        //if array at this spot == 0
+        if (gameBoard[0] == 0 && gameOver == false) {
+            console.log('topLeft clicked');
+            document.querySelector('#board0').innerHTML = 'X';
+            gameBoard[0] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board1.onclick = function topMiddle () {
+
+
+        //if array at this spot == 0
+        if (gameBoard[1] == 0 && gameOver == false) {
+            console.log('topMiddle clicked');
+            document.querySelector('#board1').innerHTML = 'X';
+            gameBoard[1] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board2.onclick = function topRight () {
+
+        //if array at this spot == 0
+        if (gameBoard[2] == 0 && gameOver == false) {
+            console.log('topRight clicked');
+            document.querySelector('#board2').innerHTML = 'X';
+            gameBoard[2] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board3.onclick = function MiddleLeft () {
+
+        //if array at this spot == 0
+        if (gameBoard[3] == 0 && gameOver == false) {
+            console.log('MiddleLeft clicked');
+            document.querySelector('#board3').innerHTML = 'X';
+            gameBoard[3] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board4.onclick = function middleMiddle () {
+
+        //if array at this spot == 0
+        if (gameBoard[4] == 0 && gameOver == false) {
+            console.log('middleMiddle clicked');
+            document.querySelector('#board4').innerHTML = 'X';
+            gameBoard[4] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board5.onclick = function middleRight () {
+
+        //if array at this spot == 0
+        if (gameBoard[5] == 0 && gameOver == false) {
+            console.log('middleRight clicked');
+            document.querySelector('#board5').innerHTML = 'X';
+            gameBoard[5] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board6.onclick = function bottomLeft () {
+
+        //if array at this spot == 0
+        if (gameBoard[6] == 0 && gameOver == false) {
+            console.log('bottomLeft clicked');
+            document.querySelector('#board6').innerHTML = 'X';
+            gameBoard[6] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board7.onclick = function bottomMiddle () {
+
+        //if array at this spot == 0
+        if (gameBoard[7] == 0 && gameOver == false) {
+            console.log('bottomMiddle clicked');
+            document.querySelector('#board7').innerHTML = 'X';
+            gameBoard[7] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    board8.onclick = function bottomRight () {
+
+        //if array at this spot == 0
+        if (gameBoard[8] == 0 && gameOver == false) {
+            console.log('bottomRight clicked');
+            document.querySelector('#board8').innerHTML = 'X';
+            gameBoard[8] = 1;
+            //console.log(gameBoard);
+            checkWinner();
+            computerTurn();
+            checkWinner();
+    
+        }
+
+        //else send message saying you cant go there
+        else {
+            console.log('you cant go here')
+        }
+    }
+
+    
+    
+
+})();
+
+
+/*
+let gameBoardController = {
+        
+
+    init: function () {
+        console.log('gbc initialted');
+    }
+};
+
+gameBoardController.init();
+
+*/
